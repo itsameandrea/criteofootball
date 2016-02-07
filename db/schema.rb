d09d9b74f2c45e431b34e77f1f10802d11dd0d02
andrearocca <andrea.rocca3@gmail.com>
2016-02-07 12:56:00 +0000

    Added role id to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207123354) do
+ActiveRecord::Schema.define(version: 20160207125401) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160207123354) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
