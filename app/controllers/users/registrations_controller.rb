2e7721b9c5090a23a93c23d5f94a3b72b6b86f68
andrearocca <andrea.rocca3@gmail.com>
2016-02-18 19:22:10 +0000

    improved look and feel

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,6 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super do |resource|
       resource.name = user_params[:name]
       resource.role_id = Role.where(:name => user_params[:role_id])[0].id
+      resource.value = 5
       resource.save
     end
   end
