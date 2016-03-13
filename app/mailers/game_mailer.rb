0ff5f176f5bed132fc261c8b839271ed8a2a9638
andrearocca <andrea.rocca3@gmail.com>
2016-03-13 20:25:13 +0000

    Improved HP

--- app/mailers/game_mailer.rb
+++ app/mailers/game_mailer.rb
@@ -1,18 +1,18 @@
 class GameMailer < ActionMailer::Base
   default to: 'andrea.rocca3@gmail.com'
   
-  def game_email(name, address, time, email)
+  def game_email(name, address, time, email, date)
     @name = name
+    @date = date 
     @address = address
     @time = time
     mail(to: email, from: "a.rocca@criteo.com", subject: '[WEEKLY FOOTBALL] - Game Subscription')
   end
   
-  def weekly_game_email(date, address, time, emails, date)
+  def weekly_game_email(date, address, time, emails)
     @date = date
     @address = address
     @time = time
-    @date = date
     
     mail(to: emails, from: "a.rocca@criteo.com", subject: "[WEEKLY FOOTBALL] New Game Created")
   end
